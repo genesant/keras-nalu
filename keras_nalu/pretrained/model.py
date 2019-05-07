35189e9b82031fd95765bc9f22fe27df1d9c05b9
Dennis Torres <djtorres0@gmail.com>
2019-05-07 15:01:16 -0400

    Add get_B function to get B tensor or base x coefficients

--- keras_nalu/pretrained/model.py
+++ keras_nalu/pretrained/model.py
@@ -2,6 +2,15 @@
 
 from os import path
 from keras.models import load_model
+import numpy as np
+
+def get_B(base, precision, size):
+    """Get B tensor of X coefficients"""
+
+    exps = range(precision[0], precision[1], -1)
+    pows = [base ** exp for exp in exps]
+    B = np.tile(pows, (size, 1))
+    return B
 
 def get_model():
     """Get the NALU pretrained model"""
--- tests/test_pretrained.py
+++ tests/test_pretrained.py
@@ -2,13 +2,12 @@
 
 from math import floor
 import numpy as np
-from keras_nalu.pretrained.model import get_model
+from keras_nalu.pretrained.model import get_B, get_model
 
-def encode(num, exps):
+def encode(num, b):
     """Encode decimal number as vector of digits"""
 
-    a = np.zeros((len(exps)))
-    b = np.asarray([10 ** exp for exp in exps])
+    a = np.zeros((len(b)))
     b_whole = np.floor(b / b[-1])
     num_whole = floor(num / b[-1])
 
@@ -33,12 +32,12 @@ def test_pretrained():
 
     model = get_model()
     nums = generate_numbers()
-    exps = list(range(7, -9, -1))
-    X = np.zeros((len(nums), 2 * len(exps)))
+    b = get_B(base=10, precision=[7, -9], size=1)[0]
+    X = np.zeros((len(nums), 2 * len(b)))
     Y = np.zeros((len(nums), 1))
 
     for i, num in enumerate(nums):
-        X[i] = encode(num, exps)
+        X[i] = encode(num, b)
         Y[i][0] = num
 
     loss = model.evaluate(x=X, y=Y)
